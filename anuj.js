7c4afb1f4f59e1c8edb55cdf1da1c40c78758d40
anujsjain11 <anujsjain11@gamil.com>
2025-02-13 03:54:14 +0530

    add:animation to devops

--- anuj.js
+++ anuj.js
@@ -3,11 +3,19 @@ document.getElementById("mainBoxHover").addEventListener("mouseover",function(){
     document.getElementById("team").classList.remove("rev");
     void team.offsetWidth;
     document.getElementById("team").classList.add("fwd");
+
+    document.getElementById("club").classList.remove("revClub");
+    void club.offsetWidth;
+    document.getElementById("club").classList.add("fwdClub");
 })
 
 document.getElementById("mainBoxHover").addEventListener("mouseleave",function(){
     document.getElementById("team").classList.remove("fwd");
     void team.offsetWidth;
     document.getElementById("team").classList.add("rev");
+    
+    document.getElementById("club").classList.remove("fwdClub");
+    void club.offsetWidth;
+    document.getElementById("club").classList.add("revClub");
 
 })
